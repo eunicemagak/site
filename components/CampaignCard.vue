e4e507d5cb809d88f8d87531532ce1c5256823c1
Celine Bowen <celine.bowen@emalify.com>
2023-06-23 16:29:06 +0300

    RW_28 Creation of the Campaign Section

---
+++ components/CampaignCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="campaign">
+        <div campaign-img>
+            <img :src="image" alt="">
+        </div>
+        <h4 class="campaign-title">
+            {{ title }}
+        </h4>
+        <div class="campaign-details" v-html="content"></div>
+
+        <nuxt-link :to="`campaign/${slug}`" class="get-campaign">Read More</nuxt-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'campaignCard',
+    props: ['image', 'content', 'slug', 'title']
+
+}
+</script>
