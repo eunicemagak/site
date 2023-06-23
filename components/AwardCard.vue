6e876ebe6c642218b0d248cfcbfbd5324710a08c
Celine Bowen <celine.bowen@emalify.com>
2023-06-23 16:21:48 +0300

    RW_30 create Awards and recognition

---
+++ components/AwardCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="award">
+        <div class="award-img">
+            <img :src="image" alt="">
+        </div>
+        <h4 class="award-title">
+            {{ title }}
+        </h4>
+        <div class="award-details" v-html="content"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'awardCard',
+    props: ['image', 'content', 'title']
+
+}
+</script>
