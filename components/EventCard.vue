fe0571b11ab6b022924ee333360f79feb647e166
Celine Bowen <celine.bowen@emalify.com>
2023-06-26 16:25:41 +0300

    RW-29 create the Upcoming events section

--- components/EventCard.vue
+++ components/EventCard.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="event">
-        <div event-img>
-            <img :src="image" alt="">
-        </div>
-        <h4 class="event-title">
-            {{ title }}
-        </h4>
-        <div class="event-details" v-html="content"></div>
+  <nuxt-link :to="`blogs/${slug}`" class="event">
+    <!-- Event image -->
+    <div event-img>
+      <img :src="image" alt="">
     </div>
+    <!-- Event title -->
+    <h4 class="event-title">
+      {{ title }}
+    </h4>
+    <!-- Event details -->
+    <div class="event-details" v-html="content"></div>
+  </nuxt-link>
 </template>
-
+  
 <script>
 export default {
-    name: 'eventCard',
-    props: ['image', 'content', 'title']
-
+  name: 'eventCard',
+  props: ['image', 'content', 'title', 'slug']
 }
 </script>
+  
