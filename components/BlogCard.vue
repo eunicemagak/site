89d93a21525f7258aff2d4937e622b872063f000
Celine Bowen <celine.bowen@emalify.com>
2023-06-26 16:23:28 +0300

    RW_27 Blog-Latest

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -1,19 +1,25 @@
 <template>
-    <div class="card blogcontent1">
-        <div class="card-image">
-            <img :src="image" alt="Image description" />
-        </div>
-        <nuxt-link :to="'blogs/' + slug" class="card-text">
-            <h3>{{ title }}</h3>
-            <div v-html="content"></div>
-            <nuxt-link :to="`blogs/${slug}`" class="button">Read more</nuxt-link>
-        </nuxt-link>
+  <div class="card blogcontent1">
+    <!-- Card image -->
+    <div class="card-image">
+      <img :src="image" alt="Image description" />
     </div>
+    <!-- Card text with link to blog -->
+    <nuxt-link :to="'blogs/' + slug" class="card-text">
+      <!-- Blog title -->
+      <h3>{{ title }}</h3>
+      <!-- Blog content -->
+      <div v-html="content"></div>
+      <!-- Read more button linked to the slug -->
+      <nuxt-link :to="`blogs/${slug}`" class="button">Read more</nuxt-link>
+    </nuxt-link>
+  </div>
 </template>
+  
 <script>
 export default {
-    name: 'blogCard',
-    props: ['image', 'content', 'slug', 'title']
-
+  name: 'blogCard',
+  props: ['image', 'content', 'slug', 'title']
 }
 </script>
+  
