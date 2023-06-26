1240199170c036e1bb7203af05680f036d280206
Celine Bowen <celine.bowen@emalify.com>
2023-06-26 16:27:06 +0300

    RW_31 Create Galley section

--- pages/Gallery.vue
+++ pages/Gallery.vue
@@ -1,34 +1,37 @@
 <template>
-  <div>
-    <div class="our-gallery">
-      <Nav />
-      <div class="gallery">
-        <div class="gallery-wrapper">
-            <h1>{{ galleryHeaderTitle }}</h1>
-            <p>{{ galleryHeaderDescription }}</p>
-        </div>
+  <div class="our-gallery">
+    <Nav />
+    <div class="gallery">
+      <div class="gallery-wrapper">
+        <h1>{{ galleryHeaderTitle }}</h1>
+        <p>{{ galleryHeaderDescription }}</p>
       </div>
-      <Blognav />
-      <div class="gallery-pics">
-        <h3>{{ galleryTitle }}</h3>
-        <div v-for="(row, index) in galleryImages" :key="index" class="flex-images">
-          <img v-for="(image, index) in row" :key="index" :src="image.src" alt="" />
-        </div>
+    </div>
+    <Blognav />
+    <div class="gallery-pics">
+      <h3>{{ galleryTitle }}</h3>
+      <div class="flex-images">
+        <img v-for="(image, index) in displayedImages" :src="image" :key="index" />
       </div>
       <div class="pagination">
-        <a href="#" class="pagination-arrow prev"
-          ><i class="fas fa-chevron-left"></i
-        ></a>
+        <a href="#" class="pagination-arrow prev" @click="previousPage">
+          <i class="fas fa-chevron-left"></i>
+        </a>
 
         <ul class="pagination-list">
           <li v-for="(page, index) in totalPages" :key="index">
-            <a href="#" class="pagination-link" :class="{ active: page === currentPage }">{{ page }}</a>
+            <a
+              href="#"
+              class="pagination-link"
+              :class="{ active: page === currentPage }"
+              @click="changePage(page)"
+            >{{ page }}</a>
           </li>
         </ul>
 
-        <a href="#" class="pagination-arrow next"
-          ><i class="fas fa-chevron-right"></i
-        ></a>
+        <a href="#" class="pagination-arrow next" @click="nextPage">
+          <i class="fas fa-chevron-right"></i>
+        </a>
       </div>
     </div>
   </div>
@@ -39,64 +42,53 @@ export default {
   data() {
     return {
       galleryHeaderTitle: 'End Of Year Party',
-      galleryHeaderDescription: 'Time to kick back and usher out 2022 and we did it in style with loads of smiles, laughter and dance.',
-      galleryTitle:'End Of Year 2022',
-      galleryImages: [
-        [
-        
-          { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image1' },
-          { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image2' },
-          { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image3' },
-    ],
-          [  
-          { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image1' },
-          { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image2' },
-          { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image3' },
-    ],
-    // [
-        
-    //       { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image1' },
-    //       { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image2' },
-    //       { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image3' },
-    // ],
-    // [
-        
-    //       { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image1' },
-    //       { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image2' },
-    //       { src: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'image3' },
-    //   ],
-    ],
-      perPage: 3,
+      galleryHeaderDescription:
+        'Time to kick back and usher out 2022 and we did it in style with loads of smiles, laughter and dance.',
+      galleryTitle: 'End Of Year 2022',
+      galleryImages: [],
+      itemsPerPage: 12,
       currentPage: 1,
-    }
+    };
   },
   computed: {
     totalPages() {
-      return Math.ceil(this.galleryImages.length / this.perPage);
+      return Math.ceil(this.galleryImages.length / this.itemsPerPage);
     },
-    currentPageImages() {
-      const startIndex = (this.currentPage - 1) * this.perPage;
-      const endIndex = startIndex + this.perPage;
+    displayedImages() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
       return this.galleryImages.slice(startIndex, endIndex);
     },
   },
-  // created() {
-  //   this.fetchGalleryImages();
-  // },
-  // // methods: {
-  // //   fetchGalleryImages() {
-  // //     this.$axios
-  // //       .get('https://localhost/wordpress/wp-json/wp/v2/gallery')
-  // //         .then(response => {
-  // //         this.galleryImages = response.data;
-  // //       })
-  // //       .catch(error => {
-  // //         console.log(error);
-  // //       });
-  // //   },
-  // },
+  created() {
+    this.fetchGalleryImages();
+  },
+  methods: {
+    fetchGalleryImages() {
+      this.$axios
+        .get('https://localhost/wordpress/wp-json/wp/v2/gallery')
+        .then((response) => {
+          this.galleryImages = response.data.map((item) => item.acf.image);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    changePage(page) {
+      this.currentPage = page;
+    },
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+  },
 };
-
 </script>
 
 
@@ -132,10 +124,12 @@ export default {
   font-weight: bold;
   text-align: center;
 }
+
 .our-gallery .gallery p {
   color: #fff;
   text-align: center;
 }
+
 .our-gallery .gallery-wrapper {
   position: absolute;
   bottom: 50px;
@@ -149,17 +143,19 @@ export default {
   font-size: 24px;
   margin: 20px 0 20px 55px;
   font-weight: bold;
+  margin-left: 110px;
 }
 
 .our-gallery .flex-images {
   display: flex;
   flex-wrap: wrap;
+  justify-content: space-between;
   justify-content: flex-start;
-  align-items: center;
+  margin: 10px 100px;
 }
 
 .our-gallery .flex-images img {
-  width: 30%;
+  width: calc(33.33% - 10px);
   margin: 5px;
   padding: 5px;
   box-sizing: border-box;
@@ -206,4 +202,6 @@ export default {
   color: #fff;
   text-decoration: none;
 }
+
+
 </style>
